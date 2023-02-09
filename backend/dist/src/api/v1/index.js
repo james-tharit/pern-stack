8913704209fbe9d1d79d361d9946f44b884cc13f
james-tharit <tharit.rt@gmail.com>
2023-02-10 00:09:21 +0700

    [REFACTOR] Remove build/ and dist/
    - Add build/ and dist/ to .gitignore

--- backend/dist/src/api/v1/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const router = express_1.Router();
-router.use("/", (req, res) => {
-    res.jsonp({ status: "ok", date: new Date().toTimeString() });
-});
-exports.default = router;
-//# sourceMappingURL=index.js.map
