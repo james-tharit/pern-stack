8845e567c578c1aa1d30f78d20c6a8c5f15f8bb9
james-tharit <tharit.rt@gmail.com>
2023-08-08 16:37:07 +0700

    update dependencies

--- scripts/start-dev-env.sh
+++ scripts/start-dev-env.sh
@@ -2,7 +2,7 @@ echo "start development server..."
 
 SCRIPT_DIR=$(cd $(dirname $0) ; pwd -P)
 cd "${SCRIPT_DIR}/../"
-echo "move to working directory..."
+echo "move to working directory...${SCRIPT_DIR}"
 
 echo "STEP 1/2: preparing persistent data"
 echo "STEP 1/2: create pgdata volume..."
