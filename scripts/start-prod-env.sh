541f8581777386f35cf991bf158e7252db68dda6
James T <tharit.rt@gmail.com>
2025-06-06 18:31:22 +0700

    refactor: updated scripts to run vite

--- scripts/start-prod-env.sh
+++ scripts/start-prod-env.sh
@@ -5,12 +5,6 @@ cd "${SCRIPT_DIR}/.."
 echo "STEP 1/4: create pgdata volume..."
 docker volume create pgdata_prod &&
 
-echo "STEP 2/4: run lint"
-sh ./scripts/run-lint.sh &&
-
-echo "STEP 3/4: run test"
-sh ./scripts/run-test.sh &&
-
 echo "STEP 4/4: run compose up"
 
 docker compose -f docker-compose.yml -f docker-compose.prod.yml up -d
